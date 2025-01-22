ae3a3ab908d7d2d92bf8efc8736ad473338194d6
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-01-22 10:21:23 +0300

    Remove templates (#1)

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Warehouse helper app",
+    short_name: "Warehouse helper",
+    description: "An application to help organize a warehouse",
+    lang: "ru",
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    start_url: "/",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
