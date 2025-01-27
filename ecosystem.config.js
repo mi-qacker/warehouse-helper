ab86556457e4ba49a45037d4d46ce997f2455c9c
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-01-27 23:22:50 +0300

    Deploy production by PM2 (#24)

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
+
+module.exports = {
+  apps: [
+    {
+      name: 'warehouse-helper-nextjs',
+      script: 'npm start',
+    },
+  ],
+  deploy: {
+    production: {
+      user: process.env.PRODUCTION_USER,
+      host: process.env.PRODUCTION_HOST,
+      ref: 'origin/main',
+      repo: 'git@github.com:mi-qacker/warehouse-helper.git',
+      path: '/var/www/warehouse-helper-frontend',
+      'post-deploy': 'npm install',
+    },
+  },
+};
