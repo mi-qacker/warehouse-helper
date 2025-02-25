426e10e7ec2db929d2c7e682f35775fd3bd6229b
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-02-25 22:31:09 +0300

    style navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,13 +3,30 @@ import Link from 'next/link';
 
 export function Navbar() {
   return (
-    <nav className="flex gap-1">
-      <h1>Warehouse Helper</h1>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href={CONFIG.DOCS_URL} target="_blank">
-        Docs
-      </Link>
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link className="text-2xl font-bold text-white" href="/">
+          Warehouse Helper
+        </Link>
+
+        <div className="flex space-x-4">
+          <NavbarLink label="Home" href="/" />
+          <NavbarLink label="Docs" href={CONFIG.DOCS_URL} blank />
+          <NavbarLink label="About" href="/about" />
+        </div>
+      </div>
     </nav>
+  );
+}
+
+function NavbarLink(props: {label: string; href: string; blank?: boolean}) {
+  return (
+    <Link
+      className="text-white hover:text-blue-200"
+      href={props.href}
+      target={props.blank ? '_blank' : undefined}
+    >
+      {props.label}
+    </Link>
   );
 }
