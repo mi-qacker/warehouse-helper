55361350661f3f860fb10921ad3996ff55bb25b6
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-03-04 15:10:13 +0300

    Create UI folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
-import {Navbar} from './Navbar';
+import {Navbar} from '@/ui/Navbar';
 import './globals.css';
 
 const geistSans = Geist({
