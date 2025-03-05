aeaea3c0ae173f254d9acfa2c951c2e733647884
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-03-05 19:58:59 +0300

    better

--- src/storages/warehouse-storage.ts
+++ src/storages/warehouse-storage.ts
@@ -1,10 +1,20 @@
 import {create} from 'zustand';
 
 export type WarehouseParams = {width: number; length: number};
+export type ShelfParams = {
+  x: number;
+  y: number;
+  width: number;
+  length: number;
+  height: number;
+  levels: number;
+};
 
 export type WarehouseStore = {
   warehouse: WarehouseParams;
   updateWarehouse: (newParams: WarehouseParams) => void;
+
+  shelving: ShelfParams[];
 };
 
 export const useWarehouseStore = create<WarehouseStore>(set => ({
@@ -12,6 +22,7 @@ export const useWarehouseStore = create<WarehouseStore>(set => ({
     width: 16,
     length: 16,
   },
-
   updateWarehouse: newParams => set({warehouse: newParams}),
+
+  shelving: [],
 }));
