2e31806d04c10df7f86e1c30ee1a19c4feb87db2
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-03-13 13:24:09 +0300

    set aside width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {Params} from '@/features/params/Params';
 export default function Page() {
   return (
     <div className="flex grow flex-row">
-      <aside>
+      <aside className="w-1/4 min-w-min">
         <Params />
       </aside>
       <main className="grow">
