04101de28c1c85daf0e8fca20b15fdf17249f24e
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-04-20 19:31:09 +0300

    export

--- src/modules/genetic-algorithm/index.ts
+++ src/modules/genetic-algorithm/index.ts
@@ -1,4 +1,8 @@
 import Genetic from 'genetic-js';
 
 const genetic = Genetic.create();
+
 genetic.select1 = Genetic.Select1.RandomLinearRank;
+genetic.select2 = Genetic.Select2.FittestRandom;
+
+export function solveOptimizationRoute() {}
