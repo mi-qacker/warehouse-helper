c01eb31b6c0835723b8db45ef9d593f801334427
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-04-21 22:36:35 +0300

    Trail banner page

--- src/app/trail/page.tsx
+++ src/app/trail/page.tsx
@@ -1,7 +1,15 @@
+import {BeakerIcon} from '@heroicons/react/24/solid';
+
 export default function TrailPage() {
   return (
-    <main>
-      <h1>Trail page</h1>
+    <main className="mx-auto w-full max-w-7xl">
+      <h1 className="my-2 text-3xl font-bold">Trail page</h1>
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-col items-center gap-4">
+          <span className="text-3xl text-gray-600">Work in progress</span>
+          <BeakerIcon className="size-12 text-emerald-600" />
+        </div>
+      </div>
     </main>
   );
 }
