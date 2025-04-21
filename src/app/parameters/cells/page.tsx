46ee67e500455ee8fb764d2b6b468364db2357f3
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-04-21 22:04:39 +0300

    Fix params pages

---
+++ src/app/parameters/cells/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import CellParams from '@/features/params/cells/CellParams';
+
+export default function CellsPage() {
+  return (
+    <main>
+      <CellParams />
+    </main>
+  );
+}
---
+++ src/app/parameters/products/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ProductParams from '@/features/params/products/ProductParams';
+
+export default function ProductsPage() {
+  return (
+    <main>
+      <ProductParams />
+    </main>
+  );
+}
