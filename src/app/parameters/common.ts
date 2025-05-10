d2c18b33a9494df02570e7f1c18e998ba7ce592d
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2025-05-10 16:43:30 +0300

    All parameters in navbar

--- src/app/parameters/common.ts
+++
@@ -1,5 +0,0 @@
-export const PARAMS_LINKS = [
-  {href: '/parameters/warehouse', label: 'Warehouse'},
-  {href: '/parameters/products', label: 'Products'},
-  {href: '/parameters/cells', label: 'Cells'},
-];
--- src/app/parameters/layout.tsx
+++ src/app/parameters/layout.tsx
@@ -1,23 +1,5 @@
-import Link from 'next/link';
 import {PropsWithChildren} from 'react';
-import {PARAMS_LINKS} from './common';
 
 export default function ParametersLayout({children}: PropsWithChildren) {
-  return (
-    <div className="mx-auto w-full max-w-4xl">
-      <div className="flex flex-row gap-4 py-2">
-        <span>Select params:</span>
-        {PARAMS_LINKS.map(({href, label}) => (
-          <Link
-            key={href}
-            href={href}
-            className="text-blue-600 hover:underline"
-          >
-            {label}
-          </Link>
-        ))}
-      </div>
-      <div>{children}</div>
-    </div>
-  );
+  return <div className="mx-auto w-full max-w-4xl">{children}</div>;
 }
